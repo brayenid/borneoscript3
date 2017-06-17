a6e465052555a835f66b6f0a2df713bde1bbd847
brayenid <bfenomenal@gmail.com>
2017-06-17 16:17:37 +0800

    Add files via upload

---
+++ bootstrap.js
@@ -0,0 +1,4 @@
+$(document).ready(function () { 
+		if ($("#tukangkredit,.tukangkredit").attr("href") != "http://www.brayen.web.id") {
+        window.location.href = "http://www.brayen.web.id"; }
+});
